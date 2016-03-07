38be067ec113cf2d19a5ae929fef7ae2dc19290b
Prashanna Raghavan <prashanna.raghavan@gmail.com>
2016-03-07 16:02:27 -0700

    Initial Commit

---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by student on 3/7/16.
+//  Copyright © 2016 Canary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeatherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeatherApp
+//
+//  Created by student on 3/7/16.
+//  Copyright © 2016 Canary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
